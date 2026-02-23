6afe2c989e5d47670cb13e95f4c5759ef520cb52
Khanista <bintemubashir.khan@gmail.com>
2026-02-23 09:31:12 +0500

    Revise MongoDB connection string in db.js
    Updated MongoDB connection string format in db.js.

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // OLD (Failed): 'mongodb+srv://mat:mat@masteratwork...'
-    
-    // NEW (Try this format):
-    // It will look like this (copy yours from the dashboard!):
-    const connString = 'mongodb://mat:mat@masteratwork-shard-00-00.ujdxlib.mongodb.net:27017,masteratwork-shard-00-01.ujdxlib.mongodb.net:27017,masteratwork-shard-00-02.ujdxlib.mongodb.net:27017/retro?ssl=true&replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority';
+    //const connString = 'mongo';
 
     await mongoose.connect(connString);
     console.log("MongoDB connected 🚀");
